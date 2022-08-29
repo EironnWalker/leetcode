089d6a39a7f97ff9edfa4500f396cd8c540153ff
eironn <eironn.walker@gmail.com>
2022-08-29 16:35:17 +0800

    feat: add net solution of ransom node

--- src/main/java/com/eironn/leetcode/ransomnode/Solution2.java
+++ src/main/java/com/eironn/leetcode/ransomnode/Solution2.java
@@ -1,38 +1,30 @@
 package com.eironn.leetcode.ransomnode;
 
 /**
- * 网友的实现
+ * 网友的实现，借助于每个字母对应的ascii码位置记录出现次数，记录每个字母出现的位置和次数，返回对比结果。
  */
 public class Solution2 {
 
     public boolean canConstruct(String ransomNote, String magazine) {
-        int[] countMagazine = new int[26];
-        int[] countRansomNote = new int[26];
-        for (int i = 0; i < magazine.length(); i++) {
-            countMagazine[magazine.charAt(i) - 'a']++;
-        }
-
+        int[] ransomArray = new int[26];
+        int[] magArray = new int[26];
         for (int i = 0; i < ransomNote.length(); i++) {
-            countRansomNote[ransomNote.charAt(i) - 'a']++;
+            ransomArray[ransomNote.charAt(i) - 'a']++;
         }
-
+        for (int i = 0; i < magazine.length(); i++) {
+            magArray[magazine.charAt(i) - 'a']++;
+        }
         for (int i = 0; i < 26; i++) {
-            if (countRansomNote[i] > countMagazine[i]) {
+            if (ransomArray[i] > magArray[i]) {
                 return false;
             }
         }
-
         return true;
     }
 
     public static void main(String[] args) {
-//        Solution2 solution1 = new Solution2();
-//        boolean b = solution1.canConstruct("121212", "11212");
-//        System.out.println(b);
-        int[] countMagazine = new int[26];
-        int i1 = 'm' - 'a';
-        countMagazine[i1]++;
-        System.out.println(i1);
-
+        Solution2 solution1 = new Solution2();
+        boolean b = solution1.canConstruct("abcde", "dcba");
+        System.out.println(b);
     }
 }
