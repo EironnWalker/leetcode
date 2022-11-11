0120c2ee4b1c535cda67baf3b114a176e90ebed8
eironn <eironn.walker@gmail.com>
2022-11-11 17:16:13 +0800

    docs: 增加描述

---
+++ src/main/java/com/eironn/leetcode/sumarray/package-info.java
@@ -0,0 +1,7 @@
+package com.eironn.leetcode.sumarray;
+/*
+ 1480 Running Sum of 1d Array
+ Input: nums = [1,2,3,4]
+ Output: [1,3,6,10]
+ Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].Ï
+ */
